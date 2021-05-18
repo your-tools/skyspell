cc24b7646a2b3a4649b1b9d5559e1f58c7c9d4d6
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-05-18 21:20:37 +0200

    Use a DB in AppDirs like a proper app

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,24 @@ use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context, Result};
+use platform_dirs::AppDirs;
 
 fn main() -> Result<()> {
-    let db_url = std::env::var("DATABASE_URL")
-        .map_err(|_| anyhow!("DATABASE_URL environment variable not set"))?;
+    let app_dirs = AppDirs::new(Some("rcspell"), false).unwrap();
+    let data_dir = app_dirs.data_dir;
+    std::fs::create_dir_all(&data_dir)
+        .with_context(|| format!("Could not create {}", data_dir.display()))?;
 
-    let _db = rcspell::db::new(&db_url)?;
     let known_words: HashSet<_> = include_str!("words_en.txt")
         .split_ascii_whitespace()
         .collect();
+
+    let db_path = &data_dir.join("en.db");
+    let db_path = db_path
+        .to_str()
+        .ok_or_else(|| anyhow!("{} contains non-UTF-8 chars", db_path.display()))?;
+    let _db = rcspell::db::new(db_path)?;
 
     let args: Vec<_> = std::env::args().collect();
     let source_path = &args[1];
