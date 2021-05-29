5ec6832c01bf5b69336de37b55e57a6df1dca1e1
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-05-29 19:58:32 +0200

    Helpful comments

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,24 @@ use anyhow::Result;
 use kak_spell::cli;
 use kak_spell::kak;
 
-fn from_kak() -> bool {
-    for (key, _) in std::env::vars() {
-        if key.starts_with("kak_") {
-            return true;
-        }
-    }
-    false
-}
-
+// The behavior of kak-spell is so different when invoked from
+// kakoune and from the command line than it's best to
+// have completely different main() functions
 fn main() -> Result<()> {
     if from_kak() {
         kak::run()
     } else {
         cli::run()
     }
+}
+
+fn from_kak() -> bool {
+    // Assume that if there's an environment variable starting with
+    // `kak_`, we are running from kakoune
+    for (key, _) in std::env::vars() {
+        if key.starts_with("kak_") {
+            return true;
+        }
+    }
+    false
 }
