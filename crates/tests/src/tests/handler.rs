f5850f53a04bb65f72b0a62f8a00fb81eeaf133b
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-04 17:21:31 +0100

    Restore RepositoryHandler tests

---
+++ crates/tests/src/tests/handler.rs
@@ -0,0 +1,25 @@
+use skyspell_core::repository::RepositoryHandler;
+
+use crate::FakeRepository;
+
+#[test]
+fn test_can_undo_file_name_skip() {
+    let repository = FakeRepository::new();
+    let mut handler = RepositoryHandler::new(repository);
+    handler.skip_file_name("foo.lock").unwrap();
+
+    handler.undo().unwrap();
+
+    assert!(!handler.is_skipped_file_name("foo.lock").unwrap());
+}
+
+#[test]
+fn test_cannot_undo_twice() {
+    let repository = FakeRepository::new();
+    let mut handler = RepositoryHandler::new(repository);
+    handler.skip_file_name("foo.lock").unwrap();
+
+    handler.undo().unwrap();
+
+    handler.undo().unwrap_err();
+}
