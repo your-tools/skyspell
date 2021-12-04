e7a2c3cd8fb0be5ae3f2ef483e12ce8119f30d4b
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-04 16:25:47 +0100

    Re-add test helpers

---
+++ crates/tests/src/lib.rs
@@ -0,0 +1,9 @@
+pub mod fake_dictionary;
+pub mod fake_interactor;
+pub mod fake_io;
+pub mod fake_repository;
+
+pub use fake_dictionary::FakeDictionary;
+pub use fake_interactor::FakeInteractor;
+pub use fake_io::FakeIO;
+pub use fake_repository::FakeRepository;
