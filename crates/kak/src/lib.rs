80b41c62155663a6f483ebf773c1078062fd8354
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-07-17 12:08:43 +0200

    Move most of skyspell-kak.rs into a kak::cli module
    This is similar to what we do in the  `skyspell` crated

--- crates/kak/src/lib.rs
+++ crates/kak/src/lib.rs
@@ -1,5 +1,7 @@
 pub(crate) mod checker;
+pub(crate) mod cli;
 pub(crate) mod io;
 
 pub use crate::checker::KakouneChecker;
+pub use cli::main;
 pub use io::{new_kakoune_io, KakouneIO, StdKakouneIO};
