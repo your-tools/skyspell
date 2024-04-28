1019b935de7b04e71aa7ec9a92c2b3776d9a811e
Dimitri Merejkowsky <dimitri@dmerej.info>
2024-04-28 13:31:57 +0200

    Rewrite tests using tmux instead of kitty

--- tests/test_kakoune_integration.py
+++ tests/test_kakoune_integration.py
@@ -1,97 +1,87 @@
 import os
-import re
 import sqlite3
 import subprocess
-import textwrap
 import time
 from pathlib import Path
-from typing import Any, Iterator, List
+from typing import Any, Iterator
 
 import pytest
 
 SOCKET_PATH = "unix:/tmp/kitty.sock"
 
 
-class KittyWindow:
-    def __init__(self, socket_path: Path) -> None:
+class TmuxSession:
+    def __init__(self, *, socket_path: Path) -> None:
         self.socket_path = socket_path
         env = os.environ.copy()
         env["PS1"] = r"\w$ "
         env["PATH"] = os.environ["PATH"]
+        print(f"tmux listening on {self.socket_path}")
+        self.session = "session"
+        self.pane = "skyspell-tests"
 
-        self.process = subprocess.Popen(
+        # fmt: off
+        subprocess.run(
             [
-                "kitty",
-                "--config",
-                "None",
-                "--class",
-                "kitty-tests",
-                "-o",
-                "allow_remote_control=yes",
-                "-o",
-                "enable_audio_bell=no",
-                "--listen-on",
-                f"unix:{self.socket_path}",
-                "sh",
+                "tmux", "-S", self.socket_path, "new-session",
+                "-s", self.session, "-n", self.pane, "-d", "sh",
             ],
             env=env,
+            check=True,
         )
-        # Wait until the kitty server is done starting
-        while not self.socket_path.exists():
-            time.sleep(0.1)
+        # fmt: on
 
-    def send_text(self, text: str) -> None:
-        cmd = [
-            "kitty",
-            "@",
-            "--to",
-            f"unix:{self.socket_path}",
-            "send-text",
-            text,
-        ]
-        subprocess.run(cmd, check=True)
+    def send_keys(self, *keys: str) -> None:
+        # fmt: off
+        subprocess.run(
+            [
+                "tmux", "-S", self.socket_path, "send-keys",
+                "-t", f"{self.session}:{self.pane}",
+                *keys,
+            ]
+        )
+        # fmt: on
 
     def get_text(self) -> str:
-        cmd = [
-            "kitty",
-            "@",
-            "--to",
-            f"unix:{self.socket_path}",
-            "get-text",
-        ]
-        process = subprocess.run(cmd, check=True, capture_output=True, text=True)
+        # fmt: off
+        process = subprocess.run(
+            [
+                "tmux", "-S", self.socket_path, "capture-pane",
+                "-t", f"{self.session}:{self.pane}", "-p",
+            ],
+            check=True,
+            capture_output=True,
+            text=True,
+        )
+        # fmt: on
         return process.stdout
 
-    def close(self) -> None:
-        self.process.kill()
+    def terminate(self) -> None:
+        subprocess.run(["tmux", "-S", self.socket_path, "kill-server"])
+
+
+@pytest.fixture()
+def tmux_session(tmp_path: Path) -> Iterator[TmuxSession]:
+    socket_path = tmp_path / "tmux.sock"
+    session = TmuxSession(socket_path=socket_path)
+    yield session
+    session.terminate()
 
 
 class RemoteKakoune:
-    def __init__(self, kitty_window: KittyWindow) -> None:
-        self.kitty_window = kitty_window
-        self.kitty_window.send_text(r"kak -n\n")
-
-    def send_keys(self, keys: str) -> None:
-        human_readable = re.sub("\x1b(.)", r"alt-\1 ", keys)
-        print(human_readable)
-        self.kitty_window.send_text(keys)
+    def __init__(self, tmux_session: TmuxSession) -> None:
+        self.tmux_session = tmux_session
+        self.tmux_session.send_keys("kak -n", "Enter")
 
     def send_command(self, command: str, *args: str) -> None:
-        self.kitty_window.send_text(r"\x1b")
-        text = rf":{command} {' '.join(args)} \n"
-        self.kitty_window.send_text(text)
-        human_readable = text.replace(r"\n", "\n")
-        print(human_readable, end="")
+        self.tmux_session.send_keys("Escape")
+        text = f":{command} " + " ".join(args)
+        print(text)
+        self.send_keys(text)
+        self.send_keys("Enter")
 
-    def extract_from_debug_buffer(self, prefix: str) -> str:
-        self.send_command("edit", "-existing", "*debug*")
-        text = self.kitty_window.get_text()
-        matching_lines = [x for x in text.splitlines() if x.startswith(prefix)]
-        # If the value has changed, we want the latest
-        line = matching_lines[-1]
-        res = line[len(prefix) :]
-        self.send_command("buffer-previous")
-        return res
+    def send_keys(self, *args: str) -> None:
+        self.tmux_session.send_keys(*args)
 
     def get_option(self, option: str) -> str:
         prefix = f"option : {option} => "
@@ -103,201 +93,254 @@ class RemoteKakoune:
         self.send_command("echo", "-debug", prefix, "%val{selection}")
         return self.extract_from_debug_buffer(prefix)
 
-
-def run_query(tmp_path: Path, sql: str) -> List[Any]:
-    db_path = tmp_path / "tests.db"
-    connection = sqlite3.connect(db_path)
-    cursor = connection.cursor()
-    cursor.execute(sql)
-    rows = cursor.fetchall()
-    return rows
-
-
-@pytest.fixture
-def kitty_window(tmp_path: Path) -> Iterator[KittyWindow]:
-    socket_path = tmp_path / "kitty.sock"
-    res = KittyWindow(socket_path=socket_path)
-    yield res
-    res.close()
+    def extract_from_debug_buffer(self, prefix: str) -> str:
+        self.send_command("edit", "-existing", "*debug*")
+        text = self.tmux_session.get_text()
+        matching_lines = [x for x in text.splitlines() if x.startswith(prefix)]
+        # If the value has changed, we want the latest
+        line = matching_lines[-1]
+        res = line[len(prefix) :]
+        self.send_command("buffer-previous")
+        return res
 
 
-@pytest.fixture
-def kak_checker(tmp_path: Path, kitty_window: KittyWindow) -> Iterator[RemoteKakoune]:
-    db_path = tmp_path / "tests.db"
-    kitty_window.send_text(rf"cd {tmp_path} \n")
+class KakChecker:
+    """
+    Represent an instance of kakoune running in a tmux session
 
-    kakoune = RemoteKakoune(kitty_window)
-    kakoune.send_command(
-        "hook",
-        "global",
-        "RuntimeError",
-        ".+",
-        "%{echo -to-file err.txt %val{hook_param}}",
-    )
-    kakoune.send_command("evaluate-commands", "%sh{ skyspell-kak init }")
-    kakoune.send_command("set-option", "global", "skyspell_db_path", str(db_path))
-    kakoune.send_command("skyspell-enable", "en_US")
+    """
 
-    yield kakoune
-
-    kakoune.send_command("quit!")
-    err_txt = tmp_path / "err.txt"
-    if err_txt.exists():
-        pytest.fail(
-            f"Some kakoune errors occurred during the test:\n{err_txt.read_text()}"
+    def __init__(self, kakoune: RemoteKakoune, tmp_path: Path) -> None:
+        self.kakoune = kakoune
+        self.tmp_path = tmp_path
+        self.db_path = tmp_path / "tests.db"
+        self.kakoune.send_command(
+            "hook",
+            "global",
+            "RuntimeError",
+            ".+",
+            "%{echo -to-file err.txt %val{hook_param}}",
         )
+        self.kakoune.send_command("evaluate-commands", "%sh{ skyspell-kak init }")
+        self.kakoune.send_command(
+            "set-option", "global", "skyspell_db_path", str(self.db_path)
+        )
+        self.kakoune.send_command("skyspell-enable", "en_US")
+
+    def debug(self) -> None:
+        subprocess.run(
+            [
+                "kitty",
+                "--detach",
+                "tmux",
+                "-S",
+                self.kakoune.tmux_session.socket_path,
+                "attach",
+            ]
+        )
+        breakpoint()
+
+    def open_file_with_contents(self, path: str, text: str) -> None:
+        full_path = self.tmp_path / path
+        full_path.write_text(text)
+        self.kakoune.send_command(f"edit %{{{path}}}")
+        self.kakoune.send_command("skyspell-check")
+
+    def open_error_list(self) -> None:
+        self.kakoune.send_command("skyspell-list")
+
+    def jump_next(self) -> None:
+        self.send_command("skyspell-next")
+
+    def jump_previous(self) -> None:
+        self.send_command("skyspell-previous")
+
+    def get_selection(self) -> str:
+        return self.kakoune.get_selection()
+
+    def send_command(self, command: str, *args: str) -> None:
+        self.kakoune.send_command(command, *args)
+
+    def send_keys(self, *args: str) -> None:
+        self.kakoune.send_keys(*args)
+
+    def ignored(self) -> list[str]:
+        rows: list[tuple[str]] = self.run_query("SELECT word FROM ignored")
+        return [r[0] for r in rows]
+
+    def ignored_for_project(self) -> list[str]:
+        rows: list[tuple[str]] = self.run_query("SELECT word FROM ignored_for_project")
+        return [r[0] for r in rows]
+
+    def ignored_for_path(self) -> list[tuple[str, str]]:
+        rows: list[tuple[str, str]] = self.run_query(
+            "SELECT word, path FROM ignored_for_path"
+        )
+        return rows
+
+    def ignored_for_extension(self) -> list[tuple[str, str]]:
+        rows: list[tuple[str, str]] = self.run_query(
+            "SELECT word, extension FROM ignored_for_extension"
+        )
+        return rows
+
+    def run_query(self, sql: str) -> list[Any]:
+        # Wait until kakoune has process the keys that were sent to the tmux pane
+        time.sleep(0.5)
+        db_path = self.tmp_path / "tests.db"
+        with sqlite3.connect(db_path) as connection:
+            cursor = connection.cursor()
+            cursor.execute(sql)
+            rows = cursor.fetchall()
+            return rows
+
+    def move_cursor(self, line: int, column: int) -> None:
+        self.kakoune.send_keys(f"{line}g")
+        self.kakoune.send_keys(f"{column}l")
+
+    def check_runtime_errors(self) -> None:
+        err_txt = self.tmp_path / "err.txt"
+        if err_txt.exists():
+            pytest.fail(
+                f"Some kakoune errors occurred during the test:\n{err_txt.read_text()}"
+            )
+
+    @property
+    def error_count(self) -> int:
+        value = self.kakoune.get_option("skyspell_error_count")
+        return int(value)
+
+    def quit(self) -> None:
+        self.kakoune.send_command("quit!")
 
 
-def open_file_with_contents(kak_checker: RemoteKakoune, path: Path, text: str) -> None:
-    path.write_text(text)
-    kak_checker.send_command(f"edit %{{{path}}}")
-    kak_checker.send_command("skyspell-check")
+@pytest.fixture
+def kak_checker(tmp_path: Path, tmux_session: TmuxSession) -> Iterator[KakChecker]:
+    tmux_session.send_keys(f"cd {tmp_path}", "Enter")
+    remote_kakoune = RemoteKakoune(tmux_session)
+    kak_checker = KakChecker(remote_kakoune, tmp_path)
+    yield kak_checker
+
+    kak_checker.check_runtime_errors()
+    kak_checker.quit()
 
 
-def test_no_spelling_errors(tmp_path: Path, kak_checker: RemoteKakoune) -> None:
-    open_file_with_contents(
-        kak_checker, tmp_path / "foo.txt", "There is no mistake there\n"
-    )
-    actual = kak_checker.get_option("skyspell_error_count")
-    assert actual == "0"
+def test_no_spelling_errors(kak_checker: KakChecker) -> None:
+    kak_checker.open_file_with_contents("foo.txt", "There is no mistake there\n")
+
+    assert kak_checker.error_count == 0
 
 
-def test_jump_to_first_error(tmp_path: Path, kak_checker: RemoteKakoune) -> None:
-    open_file_with_contents(
-        kak_checker,
-        tmp_path / "foo.txt",
+def test_jump_to_first_error(kak_checker: KakChecker) -> None:
+    kak_checker.open_file_with_contents(
+        "foo.txt",
         "There is a missstake here\nand an othhher one there",
     )
-    kak_checker.send_command("skyspell-list")
-    kak_checker.send_keys(r"\n")
+    assert kak_checker.error_count == 2
+    kak_checker.open_error_list()
+
+    kak_checker.send_keys("Enter")
     assert kak_checker.get_selection() == "missstake"
 
 
-def test_do_not_break_dot(tmp_path: Path, kak_checker: RemoteKakoune) -> None:
+def test_do_not_break_dot(tmp_path: Path, kak_checker: KakChecker) -> None:
     original_contents = "There is a missstake here\n"
     foo_path = tmp_path / "foo.txt"
-    open_file_with_contents(
-        kak_checker,
-        foo_path,
+    kak_checker.open_file_with_contents(
+        "foo.txt",
         original_contents,
     )
-    kak_checker.send_command("skyspell-list")
+    kak_checker.open_error_list()
     kak_checker.send_command(f"edit {foo_path}")
-    kak_checker.send_keys(".")
-    kak_checker.send_command("write")
+    kak_checker.kakoune.send_keys(".")
+    kak_checker.kakoune.send_command("write")
     new_contents = foo_path.read_text()
     assert original_contents == new_contents
 
 
-def test_goto_next(tmp_path: Path, kak_checker: RemoteKakoune) -> None:
-    open_file_with_contents(
-        kak_checker,
-        tmp_path / "foo.txt",
+def test_goto_next(kak_checker: KakChecker) -> None:
+    kak_checker.open_file_with_contents(
+        "foo.txt",
         "There is a missstake here\nand an othhher one there",
     )
-    kak_checker.send_keys("gg22l")
-    kak_checker.send_command("skyspell-next")
+    # Make sure we are between the first and the second error
+    kak_checker.move_cursor(1, 22)
+    kak_checker.jump_next()
     assert kak_checker.get_selection() == "othhher"
 
 
-def test_goto_previous(tmp_path: Path, kak_checker: RemoteKakoune) -> None:
-    open_file_with_contents(
-        kak_checker,
-        tmp_path / "foo.txt",
-        r"There is a missstake here\nand an othhher one there",
+def test_goto_previous(kak_checker: KakChecker) -> None:
+    kak_checker.open_file_with_contents(
+        "foo.txt",
+        "There is a missstake here\nand an othhher one there",
     )
-    kak_checker.send_keys("gg22l")
-    kak_checker.send_command("skyspell-previous")
+    # Make sure we are between the first and the second error
+    kak_checker.move_cursor(1, 22)
+    kak_checker.jump_previous()
     assert kak_checker.get_selection() == "missstake"
 
 
-def test_add_global(tmp_path: Path, kak_checker: RemoteKakoune) -> None:
-    open_file_with_contents(
-        kak_checker, tmp_path / "foo.txt", r"I'm testing skyspell here"
-    )
-
-    kak_checker.send_command("skyspell-list")
-    kak_checker.send_keys("a")
-    kak_checker.send_command("quit")
-    assert run_query(tmp_path, "SELECT word FROM ignored") == [("skyspell",)]
-
-
-def test_honor_skyspell_ignore(tmp_path: Path, kak_checker: RemoteKakoune) -> None:
+def test_honor_skyspell_ignore(tmp_path: Path, kak_checker: KakChecker) -> None:
     ignore = tmp_path / "skyspell-ignore.kdl"
     ignore.write_text("patterns {\n  foo.lock\n}\n")
-    open_file_with_contents(
-        kak_checker, tmp_path / "foo.lock", r"I'm testing skyspell here"
-    )
+    kak_checker.open_file_with_contents("foo.lock", r"I'm testing skyspell here")
 
-    kak_checker.send_command("skyspell-list")
-    actual = kak_checker.get_option("skyspell_error_count")
-    assert actual == "0"
+    kak_checker.open_error_list()
+
+    assert kak_checker.error_count == 0
 
 
-def test_add_to_project(tmp_path: Path, kak_checker: RemoteKakoune) -> None:
-    open_file_with_contents(
-        kak_checker, tmp_path / "foo.txt", r"I'm testing skyspell here"
-    )
+def test_add_global(kak_checker: KakChecker) -> None:
+    kak_checker.open_file_with_contents("foo.txt", "I'm testing skyspell here")
+    kak_checker.open_error_list()
+    kak_checker.send_keys("a")
 
-    kak_checker.send_command("skyspell-list")
+    assert kak_checker.ignored() == ["skyspell"]
+
+
+def test_add_to_project(kak_checker: KakChecker) -> None:
+    kak_checker.open_file_with_contents("foo.txt", r"I'm testing skyspell here")
+    kak_checker.open_error_list()
     kak_checker.send_keys("p")
-    kak_checker.send_command("quit")
 
-    assert run_query(tmp_path, "SELECT word FROM ignored_for_project") == [
-        ("skyspell",)
-    ]
+    assert kak_checker.ignored_for_project() == ["skyspell"]
 
 
-def test_add_to_file(tmp_path: Path, kak_checker: RemoteKakoune) -> None:
-    open_file_with_contents(
-        kak_checker, tmp_path / "foo.txt", r"I'm testing skyspell here"
-    )
-
-    kak_checker.send_command("skyspell-list")
+def test_add_to_file(kak_checker: KakChecker) -> None:
+    kak_checker.open_file_with_contents("foo.txt", r"I'm testing skyspell here")
+    kak_checker.open_error_list()
     kak_checker.send_keys("f")
-    kak_checker.send_command("quit")
 
-    assert run_query(tmp_path, "SELECT word, path FROM ignored_for_path") == [
-        ("skyspell", "foo.txt")
-    ]
+    assert kak_checker.ignored_for_path() == [("skyspell", "foo.txt")]
 
 
-def test_add_to_extension(tmp_path: Path, kak_checker: RemoteKakoune) -> None:
-    open_file_with_contents(
-        kak_checker, tmp_path / "foo.rs", "fn function(parameter: type) { body }"
-    )
+def test_add_to_extension(kak_checker: KakChecker) -> None:
+    kak_checker.open_file_with_contents("foo.rs", r"I'm testing skyspell here")
 
-    kak_checker.send_command("skyspell-list")
+    kak_checker.open_error_list()
     kak_checker.send_keys("e")
-    kak_checker.send_command("quit")
 
-    assert run_query(tmp_path, "SELECT word, extension FROM ignored_for_extension") == [
-        ("fn", "rs")
-    ]
+    assert kak_checker.ignored_for_extension() == [("skyspell", "rs")]
 
 
-def test_undo(tmp_path: Path, kak_checker: RemoteKakoune) -> None:
-    open_file_with_contents(
-        kak_checker, tmp_path / "foo.txt", r"I'm testing skyspell here"
-    )
-
-    kak_checker.send_command("skyspell-list")
+def test_undo(tmp_path: Path, kak_checker: KakChecker) -> None:
+    kak_checker.open_file_with_contents("foo.txt", "I'm testing skyspell here")
+    kak_checker.open_error_list()
     kak_checker.send_keys("a")
     kak_checker.send_keys("u")
-    kak_checker.send_command("quit")
 
-    assert run_query(tmp_path, "SELECT word FROM ignored") == []
+    assert kak_checker.ignored() == []
 
 
-def test_replace_with_suggestion(tmp_path: Path, kak_checker: RemoteKakoune) -> None:
-    open_file_with_contents(
-        kak_checker, tmp_path / "foo.txt", "There is a missstake here"
-    )
+def test_replace_with_suggestion(tmp_path: Path, kak_checker: KakChecker) -> None:
+    kak_checker.open_file_with_contents("foo.txt", "There is a missstake here")
 
-    kak_checker.send_command("skyspell-next")
+    kak_checker.jump_next()
+
     kak_checker.send_command("skyspell-replace")
-    kak_checker.send_keys(r"\n")  # select first menu entry
-    kak_checker.send_command("write-quit\n")
+    kak_checker.send_keys("Enter")  # select first menu entry
+    kak_checker.send_command("write-quit")
 
+    time.sleep(0.5)
     actual = (tmp_path / "foo.txt").read_text()
     assert actual == "There is a mistake here\n"
