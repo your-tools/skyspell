b2aa662a27f5bdb00cfa94cc91680baaf89e8e6a
Dimitri Merejkowsky <dimitri@dmerej.info>
2024-05-08 20:49:17 +0200

    Inline and delete the test_dictionary! macro ...
    and add a second test

--- crates/core/src/enchant/tests.rs
+++ crates/core/src/enchant/tests.rs
@@ -1,4 +1,15 @@
 use super::*;
-use crate::test_dictionary;
 
-test_dictionary!(EnchantDictionary);
+// Note: this test will fail if for some reason 'missstake' is in the
+// personal dict
+#[test]
+fn test_check_invalid_word() {
+    let dict = EnchantDictionary::new("en_US").unwrap();
+    assert!(!dict.check("missstake").unwrap());
+}
+
+#[test]
+fn test_check_valid_word() {
+    let dict = EnchantDictionary::new("en_US").unwrap();
+    assert!(dict.check("valid").unwrap());
+}
