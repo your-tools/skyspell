65308c3e4a722844f26e4cd6c28a204aca27d043
Dimitri Merejkowsky <dimitri@dmerej.info>
2024-10-29 14:47:54 +0100

    Draft a new target API for TokenProcessor

--- crates/core/src/tokens/tests.rs
+++ crates/core/src/tokens/tests.rs
@@ -232,3 +232,123 @@ fn test_python_string_prefix_2() {
     // TODO: this should be just ["path"]
     assert_eq!(&actual, &["r", "path"]);
 }
+
+mod target_api {
+    use std::io::{BufRead, Lines};
+
+    use super::ExtractMode;
+
+    fn get_tokens(line: &str, _extract_mode: ExtractMode) -> Vec<String> {
+        let words = line
+            .split_ascii_whitespace()
+            .map(|x| x.to_owned())
+            .collect();
+        words
+    }
+
+    struct Tokens<B> {
+        lines: Lines<B>,
+        extract_mode: ExtractMode,
+    }
+
+    impl<B> Tokens<B> {
+        fn new(lines: Lines<B>, extract_mode: ExtractMode) -> Self {
+            Self {
+                lines,
+                extract_mode,
+            }
+        }
+    }
+
+    impl<B: BufRead> IntoIterator for Tokens<B> {
+        type Item = Result<String, std::io::Error>;
+
+        type IntoIter = TokensIterator<B>;
+
+        fn into_iter(self) -> Self::IntoIter {
+            TokensIterator::new(self.lines, self.extract_mode)
+        }
+    }
+
+    struct TokensIterator<B> {
+        lines: Lines<B>,
+        extract_mode: ExtractMode,
+        #[allow(dead_code)]
+        line_number: usize,
+        current_line: String,
+        tokens: Vec<String>,
+        token_index: usize,
+        new_line: bool,
+    }
+
+    impl<B> TokensIterator<B> {
+        fn new(lines: Lines<B>, extract_mode: ExtractMode) -> Self {
+            Self {
+                lines,
+                extract_mode,
+                line_number: 0,
+                tokens: vec![],
+                token_index: 0,
+                current_line: String::new(),
+                new_line: true,
+            }
+        }
+    }
+
+    impl<B: BufRead> Iterator for TokensIterator<B> {
+        type Item = Result<String, std::io::Error>;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            loop {
+                if self.new_line {
+                    let next_line = self.lines.next();
+                    let line_result = match next_line {
+                        None => return None,
+                        Some(l) => l,
+                    };
+                    let line = match line_result {
+                        Ok(l) => l,
+                        Err(e) => return Some(Err(e)),
+                    };
+                    self.current_line = line;
+                    self.tokens = get_tokens(&self.current_line, self.extract_mode);
+                }
+
+                let token = self.tokens.get(self.token_index);
+                self.token_index += 1;
+                match token {
+                    Some(t) => {
+                        self.new_line = false;
+                        return Some(Ok(t.to_owned()));
+                    }
+                    None => {
+                        self.new_line = true;
+                        self.token_index = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn test_target_api() {
+        let text = r#" one two
+            three
+            four
+        "#;
+        let vec = text.as_bytes();
+        let lines: std::io::Lines<_> = vec.lines();
+        let tokens = Tokens::new(lines, ExtractMode::Default);
+        let tokens: Result<Vec<_>, _> = tokens.into_iter().collect();
+        let tokens = tokens.unwrap();
+        assert_eq!(
+            tokens,
+            [
+                "one".to_owned(),
+                "two".to_owned(),
+                "three".to_owned(),
+                "four".to_owned()
+            ]
+        );
+    }
+}
